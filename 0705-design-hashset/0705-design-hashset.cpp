39b35d38f0b4b0cfa4cee97917e5d40adc9d9967
GANGISETTY RAJ CHARAN <97686664+rajcharanthecoder@users.noreply.github.com>
2023-02-07 21:45:41 +0530

    Time: 674 ms (9.50%), Space: 327.9 MB (5.69%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,22 +1,63 @@
 class MyHashSet {
-public: 
-    vector<int>m;
+public:
+    //Method-1
+//     vector<int>m;
+//     int size;
+//     MyHashSet() {
+//         size = 1e6+1;
+//         m.resize(size);
+//     }
+    
+//     void add(int key) {
+//         m[key] = 1;
+//     }
+    
+//     void remove(int key) {
+//         m[key] = 0;
+//     }
+    
+//     bool contains(int key) {
+//         return m[key];
+//     }
+    
+    //Hashing Methods-------------------Chaining Method//
+    vector<list<int>>m;
     int size;
     MyHashSet() {
         size = 1e6+1;
         m.resize(size);
     }
+    int hash(int key){
+        return key%size;
+    }
+    list<int>::iterator search(int key){
+        int i = hash(key);
+        return find(m[i].begin(),m[i].end(),key); 
+    }
     
     void add(int key) {
-        m[key] = 1;
+        if(contains(key)){
+            return;
+        }
+        int i = hash(key);
+        m[i].push_back(key);
     }
     
     void remove(int key) {
-        m[key] = 0;
+         if(!contains(key)){
+            return;
+        }
+        int i = hash(key);
+        m[i].erase(search(key));
+        
     }
     
     bool contains(int key) {
-        return m[key];
+        int i = hash(key);
+        if(search(key) != m[i].end()){
+            return true;
+        }
+        return false;
     }
 };
 
