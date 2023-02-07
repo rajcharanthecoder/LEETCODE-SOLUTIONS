2e556dc94a16787f4f13351e806aca0300be3aa7
GANGISETTY RAJ CHARAN <97686664+rajcharanthecoder@users.noreply.github.com>
2023-02-07 21:50:36 +0530

    Time: 103 ms (74.03%), Space: 41 MB (84.22%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -24,29 +24,24 @@ public:
     vector<list<int>>m;
     int size;
     MyHashSet() {
-        size = 100000;
+        size = 100;
         m.resize(size);
     }
-    int hash(int key){
-        return key%size;
-    }
+    int hash(int key) {return key%size;}
+
     list<int>::iterator search(int key){
         int i = hash(key);
         return find(m[i].begin(),m[i].end(),key); 
     }
     
     void add(int key) {
-        if(contains(key)){
-            return;
-        }
+        if(contains(key)) return;
         int i = hash(key);
         m[i].push_back(key);
     }
     
     void remove(int key) {
-         if(!contains(key)){
-            return;
-        }
+         if(!contains(key)) return;
         int i = hash(key);
         m[i].erase(search(key));
         
@@ -54,9 +49,7 @@ public:
     
     bool contains(int key) {
         int i = hash(key);
-        if(search(key) != m[i].end()){
-            return true;
-        }
+        if(search(key) != m[i].end()) return true;
         return false;
     }
 };
